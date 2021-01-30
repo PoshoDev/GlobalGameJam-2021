f1ad40cad9858c399159ff9fedbd80b6fb1b9699
Jorge Figueroa Peart <54185889+JorgeFigueroaPeart@users.noreply.github.com>
2021-01-30 08:00:28 -0600

    V 0.1.2
    -Added enemy Jellyfish and its functionality

---
+++ GGJ2021/Assets/Enemy/Jellyfish/Scripts/JellyfishScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyfishScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 angle = new Vector3(.5f,0,.5f);
+
+    public float speed = 0;
+
+    void Start()
+    {
+        transform.GetComponent<Rigidbody>().AddForce(angle * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
